6464248f63c5f989da67d3abe68585762ec5b574
iamkwan <kwan1107@gmail.com>
2021-02-24 23:58:21 +0800

    Update tutorial6.py

--- tutorial6.py
+++ tutorial6.py
@@ -21,6 +21,7 @@ def webhook():
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id     = update.effective_chat.id
+        message_id  = update.message.message_id
         text        = update.message.text
         first_name  = update.effective_chat.first_name
         try:
@@ -29,11 +30,11 @@ def webhook():
             #newFile.download('test.jpg')
             # Reply with the same message
             #bot.sendMessage(chat_id=chat_id, text=f"test {bot.get_file(file_id)} {first_name}")
-            bot.sendMessage(chat_id=chat_id, text=newFile.file_path)
+            bot.sendMessage(chat_id=chat_id, reply_to_message_id=message_id, text=newFile.file_path)
             #bot.send_photo(chat_id=chat_id, photo=newFile)
             return 'ok'
         except Exception:
-            bot.sendMessage(chat_id=chat_id, text=f"{text} {first_name}")
+            bot.sendMessage(chat_id=chat_id, text=f"只限圖片")
             return 'ok'
     return 'error'
 
