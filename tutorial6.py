66850626d86598775c411946de017f67ee9f6155
iamkwan <kwan1107@gmail.com>
2021-02-24 23:52:09 +0800

    Update tutorial6.py

--- tutorial6.py
+++ tutorial6.py
@@ -25,11 +25,11 @@ def webhook():
         first_name  = update.effective_chat.first_name
         try:
             file_id = update.message.photo[-1].file_id
-            newFile = bot.get_file(file_id)
-            newFile.download('test.jpg')
+            newFile = bot.get_file(file_id).file_path
+            #newFile.download('test.jpg')
             # Reply with the same message
             #bot.sendMessage(chat_id=chat_id, text=f"test {bot.get_file(file_id)} {first_name}")
-            bot.send_photo(chat_id=chat_id, photo='https://upload.cc/i1/2021/02/09/Chdj3z.jpg')
+            bot.send_photo(chat_id=chat_id, photo=newFile)
             return 'ok'
         except Exception:
             bot.sendMessage(chat_id=chat_id, text=f"{text} {first_name}")
