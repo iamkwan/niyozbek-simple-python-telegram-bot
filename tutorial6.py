16a5246e12343d782f0a40aefe3c65acc2f8eef8
iamkwan <kwan1107@gmail.com>
2021-02-24 22:24:38 +0800

    Update tutorial6.py

--- tutorial6.py
+++ tutorial6.py
@@ -23,7 +23,9 @@ def webhook():
         chat_id     = update.effective_chat.id
         text        = update.message.text
         first_name  = update.effective_chat.first_name
-        file_id = update.message.photo[2].file_id
+        file_id = update.message.photo[-1].file_id
+        newFile = bot.get_file(file_id)
+        newFile.download('test.jpg')
         # Reply with the same message
         bot.sendMessage(chat_id=chat_id, text=f"test {bot.get_file(file_id)} {first_name}")
         return 'ok'
