89fc0e07b82089eda466e51a6ba8757ac2c7ca81
iamkwan <kwan1107@gmail.com>
2021-02-24 23:20:06 +0800

    Update tutorial6.py

--- tutorial6.py
+++ tutorial6.py
@@ -23,12 +23,16 @@ def webhook():
         chat_id     = update.effective_chat.id
         text        = update.message.text
         first_name  = update.effective_chat.first_name
-        file_id = update.message.photo[-1].file_id
-        newFile = bot.get_file(file_id)
-        newFile.download('test.jpg')
-        # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=f"test {bot.get_file(file_id)} {first_name}")
-        return 'ok'
+        try:
+            file_id = update.message.photo[-1].file_id
+            newFile = bot.get_file(file_id)
+            newFile.download('test.jpg')
+            # Reply with the same message
+            bot.sendMessage(chat_id=chat_id, text=f"test {bot.get_file(file_id)} {first_name}")
+            return 'ok'
+        except:
+            bot.sendMessage(chat_id=chat_id, text=f"test {bot.get_file(file_id)} {first_name}")
+            return 'ok'
     return 'error'
 
 def index():
